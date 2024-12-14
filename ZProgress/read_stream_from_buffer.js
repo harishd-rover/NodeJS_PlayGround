bc5ad424de76790079116a75a6fa43e13abf0924
Harish D <harishd.rover@gmail.com>
2024-12-14 17:20:50 +0530

    folder rename

--- ZProgress/read_stream_from_buffer.js
+++ ZProgress/read_stream_from_buffer.js
@@ -2,36 +2,67 @@ import * as fs from "node:fs/promises";
 import path from "node:path";
 import { Readable } from "node:stream";
 
-// Lets create a Huge Buffer
-let pathToImg = path.resolve(import.meta.dirname, "../assets/upload.jpg");
+const readStream = new Readable();
+readStream._read = () => {
+  console.log("read");
+};
 
+//* Pushing the Data to the Internal Buffer of the Readable Stream.
+readStream.push(Buffer.from("harish"));
+readStream.push(Buffer.from("harish"));
+readStream.push(Buffer.from("harish"));
+readStream.push(Buffer.from("harish"));
+readStream.push(Buffer.from("Remaining Data"));
+//* Reading the Data from the Internal Buffer.
+console.log(readStream.read(12).toString()); // reading 12 bytes
+console.log(readStream.read(12).toString()); // reading 12 bytes
+console.log(readStream.read().toString()); // read all remaining
+console.log(readStream.read()); // returns Null if no data available in internal Buffer
+
+console.log("===============================================");
+
+const timer = (value = "hello", time = 1000) =>
+  new Promise((resolve) => setTimeout(() => resolve(value), time));
+
+//* Lets create a Huge Buffer
+let pathToImg = path.resolve(import.meta.dirname, "../assets/download.txt");
 const imgBuffer = await fs.readFile(pathToImg);
-// console.log(imgBuffer);
+console.log(imgBuffer);
 
 //* const readStream = Readable.from(imgBuffer);
 //* creates huge single chunk if we pass the buffer/data
-//* if we pass asynIterator/ayncGenerator it creates proper stream.
-
 class ReadableFromBuffer extends Readable {
-  constructor(hugeBuffer = null, sizeToRead = 0) {
-    super({highWaterMark:2});
-    this.hugeBuffer = hugeBuffer;
-    this.sizeToRead = sizeToRead;
+  constructor(buffer) {
+    super({ highWaterMark: 7 });
+    this.buffer = buffer;
+    this.bufferSize = buffer.byteLength;
+    this.bytesRead = 0;
   }
-
   _construct(cb) {
-    this.push("harish");
-    this.push("harish");
-    this.push("harish");
-    this.push("harish");
     cb();
   }
-  _read(size) {
-    console.log("read");
+  
+  async _read(size) {
+    if (this.bytesRead < this.bufferSize) {
+      await timer();
+      this.push(
+        this.buffer.subarray(
+          this.bytesRead,
+          this.bytesRead + this.readableHighWaterMark
+        )
+      );
+      this.bytesRead = this.bytesRead + this.readableHighWaterMark;
+      console.log("Filled Internal Buffer");
+    } else {
+      this.push(null);
+    }
+    this.count++;
   }
 }
 
-const readStream = new ReadableFromBuffer().read();
-// readStream.on("data", (data) => {
-//   console.log("d", data);
+const readStreamFromBuffer = new ReadableFromBuffer(imgBuffer);
+console.log(readStreamFromBuffer.read());
+
+// readStreamFromBuffer.on("data", (data) => {
+//   console.log(data);
 // });
