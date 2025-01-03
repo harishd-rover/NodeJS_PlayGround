b2ee30fb2a48c1b2a61ef232051a810b0bc38fe5
Harish D <harishd.rover@gmail.com>
2025-01-03 13:39:05 +0530

    pwd vs cwd

---
+++ Tasks_Pending/playground.js
@@ -0,0 +1,4 @@
+// const buffer = Buffer.from("harish");
+
+// console.log(buffer.subarray(0, 10)); // subarray won't add any junk zeroes at end when we specify end offset more than byteLength
+
--- Tasks_Pending/temp.js
+++
@@ -1,6 +0,0 @@
-const buffer = Buffer.from("harish");
-
-console.log(buffer.subarray(0, 10)); // subarray won't add any junk zeroes at end when we specify end offset more than byteLength
-
-
-
