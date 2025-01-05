331ee9713b7164b85aa2c2b134f14d8c7d3f612a
Harish D <harishd.rover@gmail.com>
2025-01-05 14:52:30 +0530

    changes

--- Tasks_Completed_23/23.node_c_child_process.js
+++ Tasks_Completed_23/23.node_c_child_process.js
@@ -2,7 +2,7 @@ import { spawn } from "node:child_process";
 import fs from "node:fs";
 import { pipeline } from "node:stream";
 
-const c_process = spawn("../assets/num_format.exe", [
+const c_process = spawn("../assets/c_num_format.exe", [
   "../assets/num_format_output.txt",
   "$",
   ",",
@@ -19,5 +19,6 @@ pipeline(num_input_stream, c_process.stdin, (err) => {
 c_process.on("close", (code) => {
   if (code === 0) {
     console.log("C process exited with code 0.");
+    process.exit(0); // manually killing this process.
   }
 });
