8884c3a07e70c94b986ad9cc0e0f30b08137ff69
Harish D <harishd.rover@gmail.com>
2025-02-10 12:31:33 +0530

    Synchronization

--- Tasks_Completed_30/33.synchronization.js
+++ Tasks_Completed_30/33.synchronization.js
@@ -1,13 +1,13 @@
-import { Worker, threadId } from "node:worker_threads";
-import { resolve } from "node:path";
-
 //# Synchronization
 //## it is the process of organizing the concurrent threads to safely access the shared resources and critical sections to achieve the data consistency by preventing the race conditions in multi-threaded environment by using Atomics and Mutual Exclusion Techniques.
 
 //## Race conditions happens bcz of Concurrency and Paralellism of CPU.
 
-//* Concurrency => CPU context switching and HyperThreading.
-//* Parallelism => Multi-Core CPUs
+//## Concurrency => CPU context switching and HyperThreading.
+//## Parallelism => Multi-Core CPUs
+
+import { Worker, threadId } from "node:worker_threads";
+import { resolve } from "node:path";
 
 const sharedBuffer = new SharedArrayBuffer(4); // 8 * 4 = 32 Bit
 
@@ -15,6 +15,8 @@ const sharedArray = new Uint32Array(sharedBuffer);
 // reading binary data as 32bit Unsigned Integer array.
 // => Each Element => (0 to 2^32).
 // here it has only 1 element in array.
+
+const lock = new Uint8Array(new SharedArrayBuffer(1));
 
 console.log("Before: ", sharedArray);
 
@@ -28,7 +30,12 @@ const __WorkerFile = resolve(
 let completed = 0;
 
 for (let i = 0; i < No_Workers; i++) {
-  const worker = new Worker(__WorkerFile, { workerData: sharedArray.buffer });
+  const worker = new Worker(__WorkerFile, {
+    workerData: {
+      data: sharedArray.buffer,
+      lock: lock.buffer,
+    },
+  });
 
   worker.on("exit", () => {
     completed++;
--- Tasks_Completed_30/34.synchronization.worker.js
+++ Tasks_Completed_30/34.synchronization.worker.js
@@ -1,6 +1,6 @@
 import { workerData, threadId, parentPort } from "node:worker_threads";
 
-const sharedArrayBuffer = workerData;
+const sharedArrayBuffer = workerData.data;
 const sharedArray = new Uint32Array(sharedArrayBuffer);
 
 const count = 10_000_000;
@@ -9,5 +9,10 @@ for (let i = 0; i < count; i++) {
   // sharedArray[0] = sharedArray[0] + 1;  // With Race condtion
   //## Eliminating race condition by using Atomics.
   //## We have only simple operations in Atomics.
-  Atomics.add(sharedArray, 0, 1);
+  Atomics.add(sharedArray, 0, 1); // No Race Conditions with Atomics, Atomic and Isolated(Mutually Exclusive).
+  //*********************************/
+  //## Using Mutual Exclusion Techniques
+  // SpinLock
+
+  // while (condition) {}
 }
