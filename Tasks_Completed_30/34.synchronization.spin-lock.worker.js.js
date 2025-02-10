a99a16fd5290427fb4e745b16c7a0a0c80ee50e2
Harish D <harishd.rover@gmail.com>
2025-02-10 14:05:26 +0530

    spin Lock

--- Tasks_Completed_30/34.synchronization.spin-lock.worker.js.js
+++ Tasks_Completed_30/34.synchronization.spin-lock.worker.js.js
@@ -1,18 +1,48 @@
 import { workerData, threadId, parentPort } from "node:worker_threads";
 
-const sharedArrayBuffer = workerData.data;
-const sharedArray = new Uint32Array(sharedArrayBuffer);
+const sharedArray = new Uint32Array(workerData.data);
+const lock = new Uint8Array(workerData.lock);
 
 const count = 10_000_000;
 
 for (let i = 0; i < count; i++) {
-  // sharedArray[0] = sharedArray[0] + 1;  // With Race condtion
+  //## => sharedArray[0] = sharedArray[0] + 1;  // With Race condtion.
+
   //## Eliminating race condition by using Atomics.
   //## We have only simple operations in Atomics.
-  Atomics.add(sharedArray, 0, 1); // No Race Conditions with Atomics, Atomic and Isolated(Mutually Exclusive).
+  //## => Atomics.add(sharedArray, 0, 1); // No Race Conditions with Atomics, Atomic and Isolated(Mutually Exclusive).
+
   //*********************************/
   //## Using Mutual Exclusion Techniques
-  // SpinLock
+  //******* SpinLock  *******/
 
-  // while (condition) {}
+  // Isolating the critical section using Mutual Exclusion => SpinLock.
+  spinLock(lock);
+  // critical section.
+  // doing update operation on shared resource.
+  sharedArray[0] = sharedArray[0] + 1;
+  unLock(lock);
+}
+
+function spinLock(lock) {
+  const typedArray = lock;
+  const index = 0;
+  const compareValue = 0;
+  const exchangeValue = 1;
+
+  // Exchange only if comparision satisfies.
+
+  // Loops uses 100% CPU usage in all Locked Threads.
+  // So SpinLock is not an efficient method.
+  while (
+    Atomics.compareExchange(typedArray, index, compareValue, exchangeValue) ===
+    1
+  ) {}
+}
+
+function unLock(lock) {
+  const typedArray = lock;
+  const index = 0;
+  const storeValue = 0;
+  Atomics.store(typedArray, index, storeValue);
 }
